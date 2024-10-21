3cbdf2b6512a0c47c4a218d0127f786a8d31436c
PetarPetrov01 <petyr_p@mail.bg>
2024-10-21 15:01:04 +0300

    Implement not found page with ghost animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,27 @@ const config: Config = {
         skyblue: "#C3EDFF",
         lightblue: "#5BC1ED",
       },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translate(-50%,0)" },
+          "5%": { transform: "translate(-50%,0)" },
+          "20%": { transform: "translate(10%,-40px)" },
+          "60%": { transform: "translate(10%,-40px)" },
+          "90%": { transform: "translate(-50%,0)" },
+          "100%": { transform: "translate(-50%,0)" },
+        },
+        float: {
+          "0%": { transform: "translate(0,0)" },
+          "25%": { transform: "translate(0,-14px)" },
+          "50%": { transform: "translate(0,0)" },
+          "75%": { transform: "translate(0,-14px)" },
+          "100%": { transform: "translate(0,0)" },
+        },
+      },
+      animation: {
+        slide: "slide 7s infinite",
+        float: "float 2s ease-in-out infinite",
+      },
     },
   },
   plugins: [scrollBarHide],
